7aae8668fb595103e5ed487b192666f5cdce9653
PaliichukKhrystyna <159778316+PaliichukKhrystyna@users.noreply.github.com>
2024-11-01 20:06:43 +0200

    add WebAppi and BookingController

---
+++ systmHotel.WebApp/Controllers/BookingController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using systmHotel.DAL.Entities;
+
+namespace systmHotel.WebApp.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Room> booking = new List<Room>();
+            booking.Add(new Room() { RoomID=1,PricePerNight=300});
+            booking.Add(new Room() { RoomID=1,PricePerNight=300});
+            booking.Add(new Room() { RoomID=1,PricePerNight=300});
+            booking.Add(new Room() { RoomID=1,PricePerNight=300});
+            booking.Add(new Room() { RoomID=1,PricePerNight=300});
+            booking.Add(new Room() { RoomID=1,PricePerNight=300});
+
+            return View(booking);
+        }
+
+        public IActionResult AddRoom()
+        {
+            return View();
+        }
+        //public IActionResult AddRoom(Room Room)
+        //{
+        //    return View();
+        //}
+
+    }
+}
