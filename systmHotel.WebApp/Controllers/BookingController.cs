da61945b3669a554cecc310dd5cf424efb9e9712
PaliichukKhrystyna <159778316+PaliichukKhrystyna@users.noreply.github.com>
2024-11-09 15:20:41 +0200

    changed the Bookingcontroller and views

--- systmHotel.WebApp/Controllers/BookingController.cs
+++ systmHotel.WebApp/Controllers/BookingController.cs
@@ -1,31 +1,97 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using systmHotel.DAL.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace systmHotel.WebApp.Controllers
 {
     public class BookingController : Controller
     {
+        private static List<Booking> bookings = new List<Booking>();
+
         public IActionResult Index()
         {
-            List<Room> booking = new List<Room>();
-            booking.Add(new Room() { RoomID=1,PricePerNight=300});
-            booking.Add(new Room() { RoomID=2,PricePerNight=350});
-            booking.Add(new Room() { RoomID=3,PricePerNight=400});
-            booking.Add(new Room() { RoomID=4,PricePerNight=450});
-            booking.Add(new Room() { RoomID=5,PricePerNight=500});
-            booking.Add(new Room() { RoomID=6,PricePerNight=550});
+            // Повертає список бронювань
+            return View(bookings);
+        }
 
+        public IActionResult AddBooking()
+        {
+            // Повертає форму для додавання нового бронювання
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddBooking(Booking booking)
+        {
+            // Додає нове бронювання
+            booking.BookingID = bookings.Count > 0 ? bookings.Max(b => b.BookingID) + 1 : 1;
+            bookings.Add(booking);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult EditBooking(int id)
+        {
+            // Знаходить бронювання за ID та повертає форму для редагування
+            var booking = bookings.FirstOrDefault(b => b.BookingID == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
             return View(booking);
         }
 
-        public IActionResult AddRoom()
+        [HttpPost]
+        public IActionResult EditBooking(Booking booking)
         {
-            return View();
-        }
-        //public IActionResult AddRoom(Room Room)
-        //{
-        //    return View();
-        //}
+            // Оновлює дані існуючого бронювання
+            var existingBooking = bookings.FirstOrDefault(b => b.BookingID == booking.BookingID);
+            if (existingBooking == null)
+            {
+                return NotFound();
+            }
+            existingBooking.ClientID = booking.ClientID;
+            existingBooking.RoomID = booking.RoomID;
+            existingBooking.BookingDate = booking.BookingDate;
+            existingBooking.CheckInDate = booking.CheckInDate;
+            existingBooking.CheckOutDate = booking.CheckOutDate;
+            existingBooking.TotalAmount = booking.TotalAmount;
 
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteBooking(int id)
+        {
+            // Повертає форму для підтвердження видалення
+            var booking = bookings.FirstOrDefault(b => b.BookingID == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+            return View(booking);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            // Видаляє бронювання
+            var booking = bookings.FirstOrDefault(b => b.BookingID == id);
+            if (booking != null)
+            {
+                bookings.Remove(booking);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            // Повертає деталі бронювання за ID
+            var booking = bookings.FirstOrDefault(b => b.BookingID == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+            return View(booking);
+        }
     }
 }
