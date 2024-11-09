4cca4872c955ec4be5d3527d84630240de8d5119
PaliichukKhrystyna <159778316+PaliichukKhrystyna@users.noreply.github.com>
2024-11-09 15:44:25 +0200

    add ServiceController

--- systmHotel.DAL/Entities/Employee.cs
+++ systmHotel.DAL/Entities/Employee.cs
@@ -9,7 +9,7 @@ namespace systmHotel.DAL.Entities
     public class Employee
     {
         public int EmployeeId { get; set; }
-        public decimal? Name { get; set; }
+        public string? Name { get; set; }
         public string? Surname { get; set; }
         public string? Position { get; set; }
         public DateTime HireDate { get; set; }
