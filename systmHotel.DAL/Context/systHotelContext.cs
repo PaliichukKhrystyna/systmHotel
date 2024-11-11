7fd6e965ddb6ff00a2c9eaaae0a0901b51fdda1c
PaliichukKhrystyna <159778316+PaliichukKhrystyna@users.noreply.github.com>
2024-11-11 20:25:28 +0200

    fix problem

--- systmHotel.DAL/Context/systHotelContext.cs
+++ systmHotel.DAL/Context/systHotelContext.cs
@@ -16,15 +16,15 @@ namespace systmHotel.DAL.Context
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Client> Clients { get; set; }
-        // public DbSet<Position> Position { get; set; } тут чомусь помилка??
+      
 
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer("YourConnectionStringHere");
-            }
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        optionsBuilder.UseSqlServer("YourConnectionStringHere");
+        //    }
+        //}
     }
 }
